03e4f71483aca0bcdd8d4aa1fe260a39dd1559e8
Justin Searls <searls@gmail.com>
2011-05-20 14:08:55 -0400

    more tweaking

---
+++ public/js/spec-helper.js
@@ -0,0 +1,4 @@
+(function() {
+	window.context = window.describe;
+	window.xcontext = window.xdescribe;
+})();
--- public/js/try-it.js
+++ public/js/try-it.js
@@ -1,12 +1,9 @@
 (function($){
+	//Define the little iframe sandbox
 	window.Sandbox = function(){
-		var self = frames[0];
-		
+		var self = frames[0];		
 		self.eval = function(source) {
-			self.document.write(
-				'<script>'+
-					source +
-				'<\/script>');
+			self.document.write('<script>'+source+'<\/script>');
 		};
 		self.runSpecs = function() {
 			self.jasmine.getEnv().addReporter(new self.jasmine.TrivialReporter());
@@ -16,10 +13,15 @@
 		}
 		self.kill = function() {
 			$('iframe').get(0).src = $('iframe').attr('src');
-		};
-		
+		};		
 		return self;		
 	};
+	
+	//Populate default templates
+	$(function(){
+		$('#specs').val($.trim($('#default-specs').html()));
+		$('#src').val($.trim($('#default-src').html()));
+	});
 	
 	window.tryIt = function() {
 		var sandbox = Sandbox();
@@ -27,10 +29,23 @@
 		sandbox.kill();
 		$('.spec-runner').html($('body > .jasmine_reporter'));		
 	};
-	$('.try-it.button').live('click',tryIt);
-	
-	$(function(){
-		$('#specs').val($.trim($('#default-specs').html()));
-		$('#src').val($.trim($('#default-src').html()));
+	$('.try-it.button').live('click',function(e){
+		e.preventDefault();
+		tryIt();
 	});
+	$('body').live('keypress',function(e){
+		if(e.metaKey === true && e.keyCode === 13) {
+			//^ if you hit ctrl+Enter or cmd+Enter
+			tryIt();
+		}
+	});
+	$('.source').live('keydown',function(e) {
+		if(e.keyCode === 9) { //TAB			
+			e.preventDefault();
+			if(e.shiftKey !== true) {
+				$(this).insertAtCaret('  ');	
+			}
+		}
+	});
+	
 })(jQuery);
