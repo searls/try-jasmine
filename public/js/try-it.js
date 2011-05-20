f38b8f01833d1e9247b31888da904a73d05fd6a0
Justin Searls <searls@gmail.com>
2011-05-20 10:57:29 -0400

    will run now, but it's humorously bad.

--- public/js/try-it.js
+++ public/js/try-it.js
@@ -1,6 +1,15 @@
 (function($){
 	jasmine.getEnv().addReporter(new jasmine.TrivialReporter());
+	
+	
+	
 	$('.try-it.button').live('click',function(e) {
+		$('.jasmine_reporter').remove();
+		
+		eval($('#specs').val());
+		eval($('#src').val());
+		
 	  jasmine.getEnv().execute();
+		$('.jasmine_reporter').appendTo('#spec-runner');
 	});
 })(jQuery);
