15b46b5b4b825659d6c6311aae79864efc0f7652
Justin Searls <searls@gmail.com>
2011-05-22 13:01:36 -0400

    pulled out the coffee

--- Gemfile
+++ Gemfile
@@ -2,5 +2,3 @@ source 'http://rubygems.org'
 
 gem 'sinatra'
 gem 'heroku'
-gem 'coffee-script'
-gem 'rack-coffee'
--- sinatra.rb
+++ sinatra.rb
@@ -2,13 +2,6 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 
-require 'rack/coffee'
-
-use Rack::Coffee, {
-    :root => 'public',
-    :urls => ['/spec']
-}
-
 get '/' do
   redirect '/index.html'
 end
